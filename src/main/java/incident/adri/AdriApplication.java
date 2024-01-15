f57ba93f332641a9a887db7a53fe213a2a1e9319
imchopi <apchopito@gmail.com>
2024-01-15 10:13:20 +0100

    Initial Commit

---
+++ src/main/java/incident/adri/AdriApplication.java
@@ -0,0 +1,13 @@
+package incident.adri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/incident/adri/AdriApplicationTests.java
@@ -0,0 +1,13 @@
+package incident.adri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
