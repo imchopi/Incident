e82dc99e025e0d535e8284c364366460858bfb67
imchopi <apchopito@gmail.com>
2024-01-15 10:22:59 +0100

    Initial Commit

--- src/main/java/incident/Incident/ServletInitializer.java
+++ src/main/java/incident/Incident/ServletInitializer.java
@@ -1,4 +1,4 @@
-package incident.adri;
+package incident.Incident;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AdriApplication.class);
+		return application.sources(IncidentApplication.class);
 	}
 
 }
